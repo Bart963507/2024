29017f52bf5d6311d2c9207d06d068a831dd7cbd
Bart963507 <liempdbart@gmail.com>
2024-12-19 23:10:30 +0100

    first_commit

---
+++ day1.js
@@ -0,0 +1,68 @@
+"use strict";
+
+console.log("test")
+
+const list1 = []
+const list2 = []
+
+const resultArr = []
+
+
+
+function findMin(arr){
+    return Math.min(...arr)
+}
+
+function calcDiff(n1,n2){
+    return Math.abs(n1 - n2)
+}
+
+
+function removeMin(arr){
+    const index = arr.indexOf(findMin(arr))
+    arr.splice(index,1)
+}
+
+
+const input = fetch('input.txt')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.text(); // or response.json() for JSON files
+  })
+  .then(data => {
+    const stringData = data.toString()
+    const cleanData = stringData.split("   ")
+    list1.push(+cleanData[0])
+    cleanData.shift()
+    list2.push(+cleanData[cleanData.length - 1])
+    cleanData.pop()
+    console.log(cleanData)
+
+    cleanData.forEach(pair =>{
+        const splitArr = pair.split("\r\n")
+        list2.push(+splitArr[0])
+        list1.push(+splitArr[1])
+    })
+
+    while (list1.length > 0){
+        const diff = calcDiff(findMin(list1),findMin(list2))
+        resultArr.push(diff)
+        removeMin(list1)
+        removeMin(list2)
+    }
+    
+    console.log(resultArr.reduce(
+        (accumulator, currentValue) => accumulator + currentValue,
+        0
+      ))
+
+
+  })
+  .catch(error => {
+    console.error('Error reading file:', error);
+  });
+  
+
+
