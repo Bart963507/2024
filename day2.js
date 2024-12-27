91c21f87c1406c2d664d87d42b44d9fc1d9f8159
Bart963507 <liempdbart@gmail.com>
2024-12-27 23:06:51 +0100

    Restructured for part 2

--- day2.js
+++ day2.js
@@ -7,35 +7,20 @@ const input = fetch("input.txt")
   })
   .then((data) => {
     const splitData = data.toString().split("\r\n");
-    const inputsArr = JSON.parse(JSON.stringify(splitData))[0].split("\n");
+    //const inputsArr = JSON.parse(JSON.stringify(splitData))[0].split("\n");
+
+    console.log(splitData)
     const validDiffsArr = [];
     finalEqualArr = [];
-    inputsArr.forEach((d) => {
+    splitData.forEach((d) => {
       const splitArr = d.split(" ");
-      // RULE 1:
-      // The levels are either all increasing or all decreasing.
-      const positiveArr = [];
-      for (const [i, n] of splitArr.entries()) {
-        const currentNumber = splitArr[i];
-        const nextNumber = splitArr[i + 1];
-        if (nextNumber != undefined) {
-          //Push positive, negative to new array
-          const positive = checkPositive(currentNumber, nextNumber);
-          if (positive != null) {
-            positiveArr.push(positive);
-          }
-        }
-      }
-      //Create set of array. If set size is 1, then it is all either increasing or decreasing and set it as a property in the final array
-      finalEqualArr.push(checkEquality(positiveArr));
-
       //RULE 2
       //Any two adjacent levels differ by at least one and at most three.
 
       // Make new array of differences
       const diffArr = [];
 
-      //console.log(splitArr);
+    
       for (const [i, n] of splitArr.entries()) {
         const currentNumber = splitArr[i];
         const nextNumber = splitArr[i + 1];
@@ -45,6 +30,21 @@ const input = fetch("input.txt")
         }
       }
 
+  
+      const nTooBigArr = diffArr.filter(n => n > 3)
+      if (nTooBigArr.length === 1){
+        //console.log(nTooBigArr)
+        const index = diffArr.indexOf(nTooBigArr[0])
+        diffArr.splice(index,1)
+        
+        console.log(splitArr)
+        console.log(index)
+        splitArr.splice(index,1)
+        console.log(splitArr)
+        
+        
+      }
+
       const nTooBig = diffArr.findIndex((n) => n > 3);
       const zero = diffArr.indexOf(0);
       if (zero === -1 && nTooBig === -1) {
@@ -52,15 +52,37 @@ const input = fetch("input.txt")
       } else {
         validDiffsArr.push(false);
       }
-    });
-    console.log(validDiffsArr);
-    console.log(finalEqualArr);
 
+      // RULE 1:
+      // The levels are either all increasing or all decreasing.
+      let positiveArr = [];
+      for (const [i, n] of splitArr.entries()) {
+        const currentNumber = splitArr[i];
+        const nextNumber = splitArr[i + 1];
+        if (nextNumber != undefined) {
+          //Push positive, negative to new array
+          const positive = checkPositive(currentNumber, nextNumber);
+          if (positive != null) {
+            positiveArr.push(positive);
+          }
+
+        } 
+      }
+
+        // DAY 1
+    finalEqualArr.push(checkEquality(positiveArr));
+
+
+    });
+
+
+
+  
     // Join the two final arrs into one
     const combined = finalEqualArr.map(function (item, index) {
       return { equal: item, diff: validDiffsArr[index] };
     });
-    console.log(combined);
+    //console.log(combined);
     const answer = combined.filter((i) => i.equal && i.diff).length;
     console.log(answer);
   });
@@ -97,3 +119,28 @@ function checkUnEven(number1, number2) {
 function calcDiff(number1, number2) {
   Math.abs(w);
 }
+
+
+function filterArr(arr,secondArr){
+  const falseArr = arr.filter(n => n === false)
+  const trueArr = arr.filter(n => n === true)
+  //console.log(falseArr.length, trueArr.length)
+  if (falseArr.length === 1 && trueArr.length > 1){
+    const index = arr.indexOf(false)
+    secondArr.splice(index,1)
+    arr.splice(index,1)
+    return arr
+  }
+  if (falseArr.length > 1 && trueArr.length === 1){
+    const index = arr.indexOf(true)
+    arr.splice(index,1)
+    secondArr.splice(index,1)
+    return arr
+  }
+  else {
+    return arr
+  }
+
+}
+
+
